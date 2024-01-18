2cf6a262f28aec897e3aee78f94291f1c26bb7cd
artemskakun <71256755+artemskakun@users.noreply.github.com>
2024-01-18 15:51:18 +0300

    update gitignore

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Добавьте эту строку
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Middleware для обработки CORS
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
